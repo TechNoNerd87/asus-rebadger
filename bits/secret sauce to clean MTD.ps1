63c0aeef07f61152667fdd62adf449606244a28b
TechNoNerd87 <113461509+TechNoNerd87@users.noreply.github.com>
2023-08-01 17:54:08 -0500

    Update secret sauce to clean MTD.ps1
    List of expected operations list below are correct but the executed ssh is out of order

--- bits/secret sauce to clean MTD.ps1
+++ bits/secret sauce to clean MTD.ps1
@@ -14,7 +14,7 @@ pause
 
 rm  ~/.ssh/*
 
-ssh -oHostKeyAlgorithms=+ssh-dss -oKexAlgorithms=+diffie-hellman-group1-sha1 -oStrictHostKeyChecking=false admin@192.168.1.1 "cat /dev/mtd5 > /jffs/mtd5_backup.bin; mkdir /tmp/asus_jffs; mount -t jffs2 /dev/mtdblock5 /tmp/asus_jffs; sync && umount /tmp/asus_jffs; rm -rf /jffs/.sys/RT-AC68U /tmp/asus_jffs/*; nvram unset fw_check && nvram commit && reboot"
+ssh -oHostKeyAlgorithms=+ssh-dss -oKexAlgorithms=+diffie-hellman-group1-sha1 -oStrictHostKeyChecking=false admin@192.168.1.1 "cat /dev/mtd5 > /jffs/mtd5_backup.bin; mkdir /tmp/asus_jffs; mount -t jffs2 /dev/mtdblock5 /tmp/asus_jffs; rm -rf /jffs/.sys/RT-AC68U /tmp/asus_jffs/*; sync && umount /tmp/asus_jffs; nvram unset fw_check && nvram commit && reboot"
     
     
 $postsauce = @"
